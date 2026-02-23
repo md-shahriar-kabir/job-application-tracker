0be7dad6e0a428b33dd8607d1ac3583cb824d423
md-shahriar-kabir <shahriar.bizwork@gmail.com>
2026-02-23 23:40:27 +0600

    Toggle function Updated

--- script.js
+++ script.js
@@ -63,3 +63,42 @@ function calculateCount(){
     }
 }
 
+
+calculateCount();
+
+// function calculateJobs(){
+    
+// }
+
+
+function toggleStyle(id){
+    allFilterBtn.classList.remove('bg-blue-500', 'text-white');
+    interviewFilterBtn.classList.remove('bg-blue-500', 'text-white');
+    rejectedFilterBtn.classList.remove('bg-blue-500', 'text-white');
+
+    allFilterBtn.classList.add('bg-white', 'text-black/50');
+    interviewFilterBtn.classList.add('bg-white', 'text-black/50');
+    rejectedFilterBtn.classList.add('bg-white', 'text-black/50');
+    
+    const selected = document.getElementById(id);
+    currentStatus = id;
+      
+    selected.classList.remove('bg-white', 'text-black/50');
+    selected.classList.add('bg-blue-500', 'text-white');
+
+    if(id == 'interview-filter-btn'){
+        allCardSection.classList.add('hidden');
+        filterSection.classList.remove('hidden');
+        renderInterview();
+    }else if(id== 'all-filter-btn'){
+        allCardSection.classList.remove('hidden');
+        filterSection.classList.add('hidden');
+    }else if(id=='rejected-filter-btn'){
+        allCardSection.classList.add('hidden');
+        filterSection.classList.remove('hidden');
+        renderRejected();
+    }
+    calculateCount();
+}
+
+
