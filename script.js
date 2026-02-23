867be94c634fecef3fc61477479096ffd84f4a10
md-shahriar-kabir <shahriar.bizwork@gmail.com>
2026-02-23 23:43:51 +0600

    Main Function Updated

--- script.js
+++ script.js
@@ -101,4 +101,112 @@ function toggleStyle(id){
     calculateCount();
 }
 
+mainContainer.addEventListener('click', function(event){
+
+    if(event.target.classList.contains('interview-btn')){
+        const parenNode = event.target.parentNode.parentNode;
+
+        const jobsName = parenNode.querySelector('.jobsName').innerText;
+        const jobsDescription = parenNode.querySelector('.jobsDescription').innerText;
+        const selary = parenNode.querySelector('.selary').innerText;
+        const statu = parenNode.querySelector('.statu').innerText;
+        const notes = parenNode.querySelector('.notes').innerText;
+
+         const statusModifid = parenNode.querySelector('.statu');
+        statusModifid.innerText = 'INTERVIEW';
+        statusModifid.className = 'statu px-3 py-2 rounded-sm inline-block';
+        statusModifid.classList.add(
+            'text-green-600',
+            'bg-green-100',
+            'border',
+            'border-green-6000'
+        )
+
+        const cardInfo ={
+            jobsName,
+            jobsDescription,
+            selary,
+            statu:'INTERVIEW',
+            notes
+        }
+        
+        const jobsExist = interviewList.find(item=> item.jobsName == cardInfo.jobsName);
+
+                
+        if(!jobsExist){
+            interviewList.push(cardInfo)
+        }
+
+        rejectedList = rejectedList.filter(item=> item.jobsName != cardInfo.jobsName)
+                
+        calculateCount();
+
+        if(currentStatus == "rejected-filter-btn"){
+            renderRejected();
+        }
+
+        
+    }else if(event.target.classList.contains('rejected-btn')){
+        const parenNode = event.target.parentNode.parentNode;
+
+        const jobsName = parenNode.querySelector('.jobsName').innerText;
+        const jobsDescription = parenNode.querySelector('.jobsDescription').innerText;
+        const selary = parenNode.querySelector('.selary').innerText;
+        const statu = parenNode.querySelector('.statu').innerText;
+        const notes = parenNode.querySelector('.notes').innerText;
+
+        const statusModifid = parenNode.querySelector('.statu');
+        statusModifid.innerText = 'REJECTED';
+        statusModifid.className = 'statu px-3 py-2 rounded-sm inline-block';
+        statusModifid.classList.add(
+            'text-red-600',
+            'bg-red-100',
+            'border',
+            'border-red-6000'
+        )
+        
+
+        const cardInfo ={
+            jobsName,
+            jobsDescription,
+            selary,
+            statu:'REJECTED',
+            notes
+        }
+        
+        const jobsExist = rejectedList.find(item=> item.jobsName == cardInfo.jobsName);
+
+                
+        if(!jobsExist){
+            rejectedList.push(cardInfo)
+        }
+
+        interviewList = interviewList.filter(item=> item.jobsName != cardInfo.jobsName)
+
+        if(currentStatus == "interview-filter-btn"){
+            renderInterview();
+        }
+
+        calculateCount();
+    }else if(event.target.closest('.btn-delete')){
+        const cart = event.target.closest('.cart');
+        const jobsName = cart.querySelector('.jobsName').innerText;
+
+        const isconfirmed = confirm(`Are you sure to delete "${jobsName}" ?`);
+
+        if(!isconfirmed){
+            return;
+        }
+        interviewList = interviewList.filter(item => item.jobsName !== jobsName);
+        rejectedList = rejectedList.filter(item => item.jobsName !== jobsName);
+        cart.remove();
+
+        if(currentStatus === 'interview-filter-btn'){
+            renderInterview();
+        }else if(currentStatus === 'rejected-filter-btn'){
+            renderRejected();
+        }
+        calculateCount();        
+    }    
+})
 
